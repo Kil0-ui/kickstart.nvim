bc08c7b2f19498222fd963b0ec24dff817d98c9f
Jamesco <james.cortez@visualutions.com>
2024-10-10 14:34:49 -0500

    setup general list of formatters for conform to use

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -13,13 +13,6 @@ return { -- Autoformat
   },
   opts = {
     notify_on_error = false,
-    formatters_by_ft = {
-      lua = { 'stylua' },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use 'stop_after_first' to run the first available formatter from the list
-      --javascript = { 'prettierd', 'prettier', stop_after_first = true },
-    },
+    formatters = { 'stylua', 'prettier', 'prettierd' },
   },
 }
